a471c5210a0821b419abc725046354b6a3d17e7d
katout <kato@katout.com>
2013-02-09 07:39:37 +0900

    スポイト実装完了！！！
    Signed-off-by: katout <kato@katout.com>

--- Paint/src/com/katout/paint/ColorPickerDialog.java
+++ Paint/src/com/katout/paint/ColorPickerDialog.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
@@ -20,13 +21,17 @@ public class ColorPickerDialog extends Dialog implements OnSeekBarChangeListener
 	private SeekBar green;
 	private SeekBar blue;
 	private Button okButton;
+	private ImageButton dropper;
 	
 
 	// コンスタント
-	public ColorPickerDialog(Context context, OnColorChangedListener listener,
+	public ColorPickerDialog(Context context, 
 			int initialColor) {
 		super(context);
 		this.mInitialColor = initialColor;
+	}
+	
+	public void setlisner(OnColorChangedListener listener) {
 		mListener = listener;
 	}
 
@@ -90,11 +95,22 @@ public class ColorPickerDialog extends Dialog implements OnSeekBarChangeListener
 				red.setProgress(c_red);
 				green.setProgress(c_green);
 				blue.setProgress(c_blue);
-				
-				
+			}
+
+			@Override
+			public void onDropper() {
+				mListener.onDropper();
 			}
 		};
 		colorView.setup(l, mInitialColor);
+		
+		dropper = (ImageButton) findViewById(R.id.dropper);
+		dropper.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mListener.onDropper();
+			}
+		});
 	}
 	
 	@Override
